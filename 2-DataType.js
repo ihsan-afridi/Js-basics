79e3cd268ade5a4dfa82369a97b3ad01c14bbb49
mrafeidi <ihsanafridi576284@gmail.com>
2024-04-21 11:17:31 -0700

    assignment is done

---
+++ 2-DataType.js
@@ -0,0 +1,47 @@
+/* A string is a data type that represents a sequence of characters,such as letters,words or phrases.String are typically enclosed in single '' or double "".
+
+string => "This is string" , '123 abc'
+
+let name = "ihsan Afridi" */
+
+//  //  //  //  //  //
+
+/*Number: A value that represent a numerical value, like 1,2,or 3
+
+let age = 22 //this is number*/
+
+// // //  //  //  // // //
+
+/*Boolean is a data type that represent a logical value that can be either.
+
+Boolean : True/False
+let isLogged = false
+
+logical operation(AND,OR,NOT)
+loops , if else ,switch,*/
+
+//  //  //  //  //  //  //   //
+
+/*undefined is also data type 
+ let states; // output null*/
+
+ //  //    //   ///   //  //
+
+ /*null is  datatype 
+ null => standalone value*/
+
+ //  //  ///  //  //  //
+
+ /*Symbol is also data type
+
+ Symbol => unique*/
+
+ //  /// ///   ///  ///
+
+ /*Object
+
+ console.log(typeof "ihsan"); //output string
+ console.log(typeof 29);  //out out number
+ console.log(typeof null); //output object
+ console.log(typeof  undefined);// output undefined
+*/
