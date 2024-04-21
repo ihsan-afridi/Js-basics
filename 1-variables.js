be3255d899a610418a361c7fde4df09e436c74d1
mrafeidi <ihsanafridi576284@gmail.com>
2024-04-21 01:46:47 -0700

    my assignment is done

--- 1-variables.js
+++ 1-variables.js
@@ -1,35 +1,35 @@
-// Variables are Containers for Storing Data
-// JavaScript Variables can be declared in 4 ways:
+/*Variables are Containers for Storing Data
+JavaScript Variables can be declared in 4 ways:
 
-// Automatically
-// Using var
-// Using let
-// Using const
-// In this first example, x, y, and z are undeclared variables.
+Automatically
+Using var
+Using let
+Using const
+In this first example, x, y, and z are undeclared variables.
 
-// They are automatically declared when first used:
+They are automatically declared when first used:
 
 
-//Automatically
+Automatically
 x = 5;
 y = 6;
 z = x + y;
 console.log(z);
 
 
-//We change the Var value
-//Var is not block scope var is working globaly
-//prefer not to use Var
-//Because of issue in block scope and function scope
+We change the Var value
+Var is not block scope var is working globaly
+prefer not to use Var
+Because of issue in block scope and function scope
 var x = 5;
 var y = 6;
 var z = x * y;
 console.log(z);
 
 
-//let is a block scope
-//let is working in range
-//we change also the let value
+let is a block scope
+let is working in range
+we change also the let value
 let a = 5;
 let b = 6;
 let c = a - b;
@@ -37,13 +37,12 @@ let c = a - b;
 console.log(c);
 
 
-//const is also block scope
-//const value is never change
+const is also block scope
+const value is never change
 
-const afridi = 5;
+const e = 5;
 const f = 6;
-const g = afridi ** f;
+const g = e ** f;
 console.log(g);
 
-
-console.log(afridi);
+*/
